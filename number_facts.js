f4e92b7bb74ac6004a860593a61160e092f19f72
edwinkim97 <edwinkim124@gmail.com>
2022-02-14 16:20:59 -0800

    finished 1.2, working on 1.3

--- number_facts.js
+++ number_facts.js
@@ -1,15 +1,38 @@
 "use strict";
 
+// Get single number fact
 async function getNumFact() {
   const resp = await axios.get("http://numbersapi.com/42?json");
-  console.log(resp);
-  console.log(resp.data.text);
+  // console.log(resp);
+  // console.log(resp.data.text);
+  return resp.data.text;
 }
 
+// Get fact for multiple numbers
 async function getMultiNumFacts() {
-  header = { "Content-Type": "application/json" };
-  const url = "";
+  const header = { "Content-Type": "application/json" };
+  const url = "http://numbersapi.com/1..3";
   const resp = await axios.get(url, { headers: header });
+  console.log(resp);
+  
+  for (let val of Object.values(resp.data)){
+    let fact = $(`<li>${val}</li>`);
+    $("#facts").append(fact);
+  }
 }
 
-getNumFact();
+// Get 4 facts for single number
+async function get4NumFacts() {
+  let p1 = getNumFact();
+  let p2 = getNumFact();
+  let p3 = getNumFact();
+  let p4 = getNumFact();
+
+  let results = [await p1, await p2, await p3, await p4];
+
+  
+}
+
+
+// getNumFact();
+getMultiNumFacts();
